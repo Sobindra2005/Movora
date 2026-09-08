95a7d0fcbd3268bf3ff37d55e11d2e1805be4f68
sobhindra <sobhindra2005@gmail.com>
2026-09-08 20:37:21 +0545

    feat: add Hero component and integrate it into Home page; update NavBar styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,11 @@
+import { Hero } from "../components/hero";
+
 export function Home(){
     return (
         <div className="h-full overflow-hidden">
+            <Hero/>
+
+            {/* <Trending/> */}
        
         </div>
     )
