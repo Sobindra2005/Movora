44f6afab9b8667df99c3730059d5e69e206000c2
sobhindra <sobhindra2005@gmail.com>
2026-09-08 20:42:59 +0545

    feat: add Trending component and integrate it into Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,11 @@
 import { Hero } from "../components/hero";
+import { Trending } from "../components/trending";
 
 export function Home(){
     return (
         <div className="h-full overflow-hidden">
             <Hero/>
-
-            {/* <Trending/> */}
-       
+            <Trending/>
         </div>
     )
 }
