5c09b90de607d18791ef61dae74ed755019557eb
sobhindra <sobhindra2005@gmail.com>
2026-09-09 19:19:34 +0545

    feat: add API configuration for TMDB using axios

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseUrl = 'https://api.themoviedb.org/3';
+const tmdbApi = import.meta.env.VITE_TMDB_API_KEY
+
+export const movieApi = axios.create({
+    baseURL: baseUrl,
+    params: {
+        api_key: tmdbApi
+    }
+})
